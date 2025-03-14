c8069f340d3dc8eb3a57281cdd4f4bdfe9d67b34
Theodore Kranz <kranzt17@muhs.edu>
2025-03-13 20:35:27 -0500

    fix error

--- src/main/java/frc/robot/subsystems/intake_subsystem/Intake.java
+++ src/main/java/frc/robot/subsystems/intake_subsystem/Intake.java
@@ -78,7 +78,8 @@ public class Intake extends SubsystemBase {
     intakeMap.put(ArmevatorPose.ALGAE_L2_PLUCK, 5.0);
     intakeMap.put(ArmevatorPose.ALGAE_L2_DROP, 10.0);
     intakeMap.put(ArmevatorPose.ALGAE_L2_PLUCK, 5.0);
-    intakeMap.put(ArmevatorPose.ALGAE_PRE_PLUCK, 5.0);
+    intakeMap.put(ArmevatorPose.ALGAE_PRE_PLUCK_L2, 5.0);
+    intakeMap.put(ArmevatorPose.ALGAE_PRE_PLUCK_L2, 5.0);
 
     TalonFXConfiguration intakeConfig = new TalonFXConfiguration();
     intakeConfig.MotorOutput.withInverted(InvertedValue.CounterClockwise_Positive);
